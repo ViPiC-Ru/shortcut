56bd806d14f02560412196360342e21831b0e25c
Savenkov Ivan <vip-ic@mail.ru>
2022-05-28 14:29:39 +0300

    Переписал с использованием библиотек.

--- shortcut.js
+++ shortcut.js
@@ -1,215 +1,180 @@
-/* 0.1.1 создание, изменение и удаление ярлычка
+/* 0.2.0 создание, изменение и удаление ярлычка
 
-cscript shortcut.min.js <location> <name> <target> [<argument> [<directory> [<description> [<icon> [<style> [<hotkey>]]]]]]
+cscript shortcut.min.js <location> <name> [<target> [<argument> [<directory> [<description> [<icon> [<style> [<hotkey>]]]]]]]
 
-<location>      - путь к существующей папке, в которой нужно создать ярлычок (доступны ключи WshSpecialFolder)
-<name>          - имя ярлычка без расширения, при несовпадении регистра ярлычок переименовывается
-<target>        - путь к папке или файлу, на который создаётся ярлычок, при отсутствие целевого объекта ярлычок удаляется (доступны ключи WshSpecialFolder)
-<argument>      - аргументы целевого объекта, одинарные кавычки заменяются двойными
-<directory>     - путь к существующей рабочей папке (доступны ключи WshSpecialFolder)
-<description>   - описание ярлычка, которое записывается в подсказку
-<icon>          - путь к файлу и индекс иконки отделённый запятой
-<style>         - целое число, как стиль запуска окна приложения
-<hotkey>        - комбинация горячих клавиш
+<location>      - Путь к существующей папке, в которой нужно создать ярлычок (доступны ключи WshSpecialFolder).
+<name>          - Имя ярлычка без расширения, при несовпадении регистра ярлычок переименовывается.
+<target>        - Путь к целевому файлу, папке или url, при отсутствии ярлычок удаляется (доступны ключи WshSpecialFolder).
+<argument>      - Аргументы для целевого объекта, одинарные кавычки заменяются двойными.
+<directory>     - Путь к существующей рабочей папке (доступны ключи WshSpecialFolder).
+<description>   - Описание ярлычка, которое отображается в подсказке.
+<icon>          - Путь к файлу и индекс иконки, отделённый запятой.
+<style>         - Стиль запуска окна приложения (1 - Normal, 3 - Maximized, 7 - Minimized).
+<hotkey>        - Комбинация горячих клавиш.
 
 */
 
-(function (wsh, undefined) {// замыкаем что бы не сорить глобалы
-    var value, flag, pattern, shell, fso, shortcut, file, location, name,
-        target, argument, directory, description, icon, style, hotkey,
-        delim = ".", ext = "lnk", error = 0;
+var shortcut = new App({
+    lnkExt: "lnk",                  // расширение для стандартного ярлычка
+    urlExt: "url",                  // расширение для интернет ярлычка
+    escСhar: "^",                   // экранирующий символ
+    extDelim: "."                   // разделитель для расширеня
+});
 
-    shell = new ActiveXObject("WScript.Shell");
-    fso = new ActiveXObject("Scripting.FileSystemObject");
-    // получаем строковые значения параметров
-    if (!error) {// если нет ошибок
-        for (var i = 0, iLen = wsh.arguments.length; i < iLen; i++) {
-            value = wsh.arguments.item(i);// получаем очередное значение
-            switch (i) {// поддерживаемые параметры
-                case 0: location = value; break;
-                case 1: name = value; break;
-                case 2: target = value; break;
-                case 3: argument = value; break;
-                case 4: directory = value; break;
-                case 5: description = value; break;
-                case 6: icon = value; break;
-                case 7: style = value; break;
-                case 8: hotkey = value; break;
+// подключаем зависимые свойства приложения
+(function (wsh, app, undefined) {
+    app.lib.extend(app, {
+        fun: {// зависимые функции частного назначения
+        },
+        init: function () {// функция инициализации приложения
+            var key, value, name, index, location, target, length, list, ext, fso,
+                shell, path, data, isFound, isUrl, config = {}, error = 0;
+
+            shell = new ActiveXObject("WScript.Shell");
+            fso = new ActiveXObject("Scripting.FileSystemObject");
+            // получаем параметры
+            if (!error) {// если нет ошибок
+                length = wsh.arguments.length;// получаем длину
+                for (index = 0; index < length; index++) {// пробигаемся по параметрам
+                    value = wsh.arguments.item(index);// получаем очередное значение
+                    // путь к папке расположения ярлычка
+                    key = "location";// ключ проверяемого параметра
+                    if (0 == index && !(key in config)) {// если нужно выполнить
+                        // корректируем значение
+                        path = value ? shell.specialFolders(value) : null;
+                        if (path) value = path;// если папка задана ключём WshSpecialFolder
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // имя ярлычка без расширения
+                    key = "name";// ключ проверяемого параметра
+                    if (1 == index && !(key in config)) {// если нужно выполнить
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // путь к целевому объекту
+                    key = "target";// ключ проверяемого параметра
+                    if (2 == index && !(key in config)) {// если нужно выполнить
+                        // корректируем значение
+                        path = value ? shell.specialFolders(value) : null;
+                        if (path) value = path;// если папка задана ключём WshSpecialFolder
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // аргументы целевого объекта
+                    key = "argument";// ключ проверяемого параметра
+                    if (3 == index && !(key in config)) {// если нужно выполнить
+                        // корректируем значение
+                        value = value.split("'").join('"');
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // путь к рабочей папке
+                    key = "directory";// ключ проверяемого параметра
+                    if (4 == index && !(key in config)) {// если нужно выполнить
+                        // корректируем значение
+                        path = value ? shell.specialFolders(value) : null;
+                        if (path) value = path;// если папка задана ключём WshSpecialFolder
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // описание ярлычка
+                    key = "description";// ключ проверяемого параметра
+                    if (5 == index && !(key in config)) {// если нужно выполнить
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // путь к файлу и индекс иконки
+                    key = "icon";// ключ проверяемого параметра
+                    if (6 == index && !(key in config)) {// если нужно выполнить
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // стиль запуска окна приложения
+                    key = "style";// ключ проверяемого параметра
+                    if (7 == index && !(key in config)) {// если нужно выполнить
+                        // корректируем значение
+                        value = Number(value);
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // комбинация горячих клавиш
+                    key = "hotkey";// ключ проверяемого параметра
+                    if (8 == index && !(key in config)) {// если нужно выполнить
+                        // присваиваем значение
+                        config[key] = value;// задаём значение
+                        continue;// переходим к следующему параметру
+                    };
+                    // если закончились параметры конфигурации
+                    break;// остававливаем получние параметров
+                };
             };
-        };
-    };
-    // проверяем местоположение
-    if (!error) {// если нету ошибок
-        // преобразовываем путь
-        value = location ? shell.specialFolders(location) : null;
-        if (!value) {// если папка задана не ключём WshSpecialFolder
-            value = shell.expandEnvironmentStrings(location);
-            value = fso.getAbsolutePathName(value);
-        } else location = value;// задаём путь
-        // проверяем путь
-        if (fso.folderExists(value)) {// если папка существует
-        } else if (fso.fileExists(value)) {// если файл фуществует
-        } else error = 1;
-    };
-    // проверяем название
-    if (!error) {// если нету ошибок
-        pattern = /[\\\/:\*\?"<>\|]/;// запрещённые знаки
-        if (name && !pattern.test(name)) {// если проверка пройдена
-        } else error = 2;
-    };
-    // получаем ярлычок
-    if (!error) {// если нету ошибок
-        value = name + delim + ext;// имя ярлычка
-        value = fso.buildPath(location, value);
-        shortcut = shell.createShortcut(value);
-    };
-    // проверяем назначение и удаляем ярлычок если нужно
-    if (!error) {// если нету ошибок
-        // преобразовываем путь
-        value = target ? shell.specialFolders(target) : null;
-        if (!value) {// если папка задана не ключём WshSpecialFolder
-            value = shell.expandEnvironmentStrings(target);
-            value = fso.getAbsolutePathName(value);
-        } else target = value;// задаём путь
-        // проверяем путь
-        if (fso.folderExists(value)) {// если папка существует
-        } else if (fso.fileExists(value)) {// если файл фуществует
-        } else {// если проверка не пройдена
-            // пробуем удалить ярлычок
-            if (shortcut.targetPath) {// если ярлычок существует
-                try {// пробуем выполнить комманду
-                    fso.deleteFile(shortcut.fullName);
-                } catch (e) { };// игнорируем исключения
+            // проверяем обязательные поля
+            if (!error) {// если нет ошибок
+                if (("location" in config) && ("name" in config)) {
+                } else error = 1;
             };
-            error = 3;
-        };
-    };
-    // корректируем аргументы
-    if (!error && argument) {// если нужно выполнить
-        argument = argument.split("'").join('"');
-    };
-    // проверяем рабочую папку
-    if (!error && directory) {// если нужно выполнить
-        // преобразовываем путь
-        value = directory ? shell.specialFolders(directory) : null;
-        if (!value) {// если папка задана не ключём WshSpecialFolder
-            value = shell.expandEnvironmentStrings(directory);
-            value = fso.getAbsolutePathName(value);
-        } else directory = value;// задаём путь
-        // проверяем путь
-        if (fso.folderExists(value)) {// если папка существует
-        } else error = 4;
-    };
-    // проверяем стиль запуска
-    if (!error && style) {// если нужно выполнить
-        switch (style) {// поддерживаемые стили
-            case "1": case "3": case "7": style = 1 * style; break;
-            default: error = 5;
-        };
-    };
-    // задаём назначение
-    if (!error && "undefined" != typeof target) {// если нужно выполнить
-        value = shell.expandEnvironmentStrings(target);
-        value = fso.getAbsolutePathName(value).toLowerCase();
-        if (shortcut.targetPath.toLowerCase() != value) {
-            try {// пробуем выполнить комманду
-                shortcut.targetPath = target;
-                flag = true;
-            } catch (e) {// если ошибка
-                error = 6;
+            // проверяем стиль запуска
+            if (!error && ("style" in config)) {// если нужно выполнить
+                if (app.lib.hasValue([1, 3, 7], config.style, false)) {
+                } else error = 2;
             };
-        };
-    };
-    // задаём аргументы
-    if (!error && "undefined" != typeof argument) {// если нужно выполнить
-        if (shortcut.arguments != argument) {
-            try {// пробуем выполнить комманду
-                shortcut.arguments = argument;
-                flag = true;
-            } catch (e) {// если ошибка
-                error = 7;
+            // работаем с ярлычком
+            if (!error) {// если нет ошибок
+                data = {};// данные для создания ярлычка
+                isUrl = config.target && ~config.target.indexOf("/");
+                // формируем путь к расположению ярлычка
+                isFound = false;// найден ли существующий ярлык
+                if (!config.target) list = [app.val.urlExt, app.val.lnkExt];
+                else list = [isUrl ? app.val.urlExt : app.val.lnkExt];
+                for (var i = 0, iLen = list.length; i < iLen && !isFound; i++) {
+                    ext = list[i];// получаем очередное расширение
+                    name = config.name + app.val.extDelim + ext;// имя с расширением
+                    path = fso.buildPath(config.location, name);// путь к ярлычку
+                    path = path.split(app.val.escСhar).join("");
+                    location = shell.expandEnvironmentStrings(path);
+                    if (fso.fileExists(location)) isFound = true;
+                };
+                data.fullName = path;
+                // формируем путь к целевому объекту
+                path = config.target;
+                if (path && !isUrl) {// если есть целевой объект и это не url
+                    path = path.split(app.val.escСhar).join("");
+                    target = shell.expandEnvironmentStrings(path);
+                    if (!fso.fileExists(target)) path = null;
+                };
+                if ("target" in config) data.targetPath = path;
+                // исправляем экранирование в других параметрах
+                for (var key in config) {// пробигаемся по параметрам
+                    if (app.lib.hasValue(["argument", "directory", "icon"], key, true)) {
+                        path = config[key];// получаем очередное значение
+                        path = path.split(app.val.escСhar).join("");
+                        config[key] = path;
+                    };
+                };
+                // формируем остальные параметры
+                if ("argument" in config) data.arguments = config.argument;
+                if ("directory" in config) data.workingDirectory = config.directory;
+                if ("description" in config) data.description = config.description;
+                if ("icon" in config) data.iconLocation = config.icon;
+                if ("style" in config) data.windowStyle = config.style;
+                if ("hotkey" in config) data.hotkey = config.hotkey;
+                // выполняем действие с ярлычком
+                if (obj = app.wsh.setShortcut(data)) {// если выполнено успешно
+                } else error = 3;
             };
-        };
-    };
-    // задаём рабочую папку
-    if (!error && "undefined" != typeof directory) {// если нужно выполнить
-        if (shortcut.workingDirectory != directory) {
-            try {// пробуем выполнить комманду
-                shortcut.workingDirectory = directory;
-                flag = true;
-            } catch (e) {// если ошибка
-                error = 8;
-            };
-        };
-    };
-    // задаём иконку
-    if (!error && "undefined" != typeof icon) {// если нужно выполнить
-        if (!~icon.indexOf(",")) value = icon + ",0";
-        else value = icon.split(", ").join(",");
-        if (shortcut.iconLocation != value) {
-            try {// пробуем выполнить комманду
-                shortcut.iconLocation = value;
-                flag = true;
-            } catch (e) {// если ошибка
-                error = 9;
-            };
-        };
-    };
-    // задаём стиль запуска
-    if (!error && "undefined" != typeof style) {// если нужно выполнить
-        value = style ? style : 1;
-        if (shortcut.windowStyle != value) {
-            try {// пробуем выполнить комманду
-                shortcut.windowStyle = value;
-                flag = true;
-            } catch (e) {// если ошибка
-                error = 10;
-            };
-        };
-    };
-    // задаём горячие клавиши
-    if (!error && "undefined" != typeof hotkey) {// если нужно выполнить
-        if (shortcut.hotkey != hotkey) {
-            try {// пробуем выполнить комманду
-                shortcut.hotkey = hotkey;
-                flag = true;
-            } catch (e) {// если ошибка
-                error = 11;
-            };
-        };
-    };
-    // задаём описание
-    if (!error && "undefined" != typeof description) {// если нужно выполнить
-        if (shortcut.description != description) {
-            try {// пробуем выполнить комманду
-                shortcut.description = description;
-                flag = true;
-            } catch (e) {// если ошибка
-                error = 12;
-            };
-        };
-    };
-    // пробуем сохранить изменения
-    if (!error && flag) {// если нужно выполнить
-        try {// пробуем выполнить комманду
-            shortcut.save();
-        } catch (e) {// если ошибка
-            error = 13;
-        };
-    };
-    // переименовываем ярлычок
-    if (!error) {// если нету ошибок
-        value = name + delim + ext;// имя ярлычка
-        file = fso.getFile(shortcut.fullName);
-        if (file.name != value) {
-            value = fso.buildPath(location, value);
-            try {// пробуем выполнить комманду
-                fso.moveFile(file.path, value);
-            } catch (e) {// если ошибка
-                error = 14;
-            };
-        };
-    };
-    // завершаем сценарий кодом
-    wsh.quit(error);
-})(WSH);
+            // завершаем сценарий кодом
+            wsh.quit(error);
+        }
+    });
+})(WSH, shortcut);
+// запускаем инициализацию
+shortcut.init();
